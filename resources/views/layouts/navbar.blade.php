912f4f0de4e007debe34105a35e7e5f579d7143b
Patrice-Lumumba <wapalu2004@gmail.com>
2024-03-06 10:35:09 +0100

    update user

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -11,7 +11,7 @@
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/users">Users</a>
+                    <a class="nav-link" href="/user">Users</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/etudiant">Etudiant</a>
